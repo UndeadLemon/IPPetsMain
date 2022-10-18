73fd511be15bfa114ca2cb59c093a0cdfd5b84f0
UndeadLemon <thane.abel@gmail.com>
2022-10-18 17:34:18 -0500

    Upgraded UI, new pages and buttons added

--- ip_pets/controllers/pets.py
+++ ip_pets/controllers/pets.py
@@ -1,6 +1,8 @@
+from math import floor
 from ip_pets import app, bcrypt
 from ip_pets.models.pet import Pet
 from ip_pets.models.SQL import SQL
+from datetime import datetime
 from flask import render_template,redirect,request,session,flash
 
 @app.route("/")
@@ -24,7 +26,43 @@ def this_ip_pet():
         pet = Pet.get_one(data)
     else:
         pet = Pet.get_one(data)
-    timer = pet.last_fed_at
+        
+    if pet.process_update():
+        return redirect('/this_ip_pet')
+    print(pet.last_fed_at)
+    time_difference = datetime.now() - pet.last_fed_at
+    timer = floor(time_difference.seconds/3600)
     
-    return render_template('index.html',timer=timer, pet=pet,ip=ip)
+    # d = datetime.strptime(pet.last_fed_at, '%Y-%m-%d %H:%M:%S')
+    
+    
+    return render_template('pet_page.html',timer=timer, pet=pet,ip=ip)
 
+@app.route('/feed_pet')
+def feed_pet():
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        ip = str(request.environ['REMOTE_ADDR'])
+    else:
+        ip = str(request.environ['HTTP_X_FORWARDED_FOR'])
+
+    data = {
+        'pet_ip':ip
+    }
+    pet = Pet.get_one(data)
+    pet.feed_pet()
+    return redirect('/this_ip_pet')
+
+@app.route('/pet_stats')
+def pet_stats():
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        ip = str(request.environ['REMOTE_ADDR'])
+    else:
+        ip = str(request.environ['HTTP_X_FORWARDED_FOR'])
+
+    data = {
+        'pet_ip':ip
+    }
+    pet = Pet.get_one(data)
+    
+    last_fed = pet.last_fed_at.strftime('%m/%d, %Y, at %I:%S')
+    return render_template('pet_stats.html', pet=pet, last_fed=last_fed)
--- ip_pets/models/pet.py
+++ ip_pets/models/pet.py
@@ -1,7 +1,8 @@
 from ip_pets.config.mysqlconnection import connectToMySQL
 from ip_pets import DB
 from ip_pets.models.SQL import SQL
-import random, re, datetime, requests
+from datetime import datetime
+import random, re, requests, asyncio
 
 class Pet(SQL):
     table = "pets"
@@ -12,6 +13,23 @@ class Pet(SQL):
         self.name = data['name']
         self.pet_ip = data['pet_ip']
         self.pet_image = data['pet_image']
+
+    def process_update(self):
+        hunger = datetime.now() - self.last_fed_at
+        if (hunger.seconds/3600) > self.feed_timer:
+            data = {
+                'pet_ip':self.pet_ip
+            }
+            return self.delete(data)
+    def feed_pet(self):
+        data = {
+            'pet_ip':self.pet_ip
+            }
+        query = """UPDATE pets
+        SET last_fed_at = NOW()
+        WHERE pet_ip = %(pet_ip)s
+        """
+        return connectToMySQL(DB).query_db(query, data)
 
     @classmethod
     def initialize_pet(cls, data):
@@ -38,3 +56,7 @@ class Pet(SQL):
         names = ["Abigail","Ace","Adam","Addie","Admiral","Aggie","Aires","Aj","Ajax","Aldo","Alex","Alexus","Alf","Alfie","Allie","Ally","Amber","Amie","Amigo","Amos","Amy","Andy","Angel","Angus","Annie","Apollo","April","Archie","Argus","Aries","Armanti","Arnie","Arrow","Ashes","Ashley","Astro","Athena","Atlas","Audi","Augie","Aussie","Austin","Autumn","Axel","Axle","Babbles","Babe","Baby","Baby-doll","Babykins","Bacchus","Bailey","Bam-bam","Bambi","Bandit","Banjo","Barbie","Barclay","Barker","Barkley","Barley","Barnaby","Barney","Baron","Bart","Basil","Baxter","Bb","Beamer","Beanie","Beans","Bear","Beau","Beauty","Beaux","Bebe","Beetle","Bella","Belle","Ben","Benji","Benny","Benson","Bentley","Bernie","Bessie","Biablo","Bibbles","Big Boy","Big Foot","Biggie","Billie","Billy","Bingo","Binky","Birdie","Birdy","Biscuit","Bishop","Gus","Guy","Gypsy","Hailey","Haley","Hallie","Hamlet","Hammer","Hank","Hanna","Hannah","Hans","Happy","Hardy","Harley","Harpo","Harrison","Harry","Harvey","Heather","Heidi","Henry","Hercules","Hershey","Higgins","Hobbes","Holly","Homer","Honey","Honey-Bear","Hooch","Hoover","Hope","Houdini","Howie","Hudson","Huey","Hugh","Hugo","Humphrey","Hunter","India","Indy","Iris","Isabella","Isabelle","Itsy","Itsy-bitsy","Ivory","Ivy","Izzy","Jack","Jackie","Jackpot","Jackson","Jade","Jagger","Jags","Jaguar","Jake","Jamie","Jasmine","Jasper","Jaxson","Jazmie","Jazz","Jelly","Jelly-bean","Jenna","Jenny","Jerry","Jersey","Jess","Jesse","Jesse James","Jessie","Jester","Jet","Jethro","Jett","Jetta","Jewel","Jewels","Jimmy","Jingles","JJ","Joe","Joey","Johnny","Jojo","Joker","Jolie","Jolly","Jordan","Josie","Joy","JR","Judy","Julius","June","Misty","Mitch","Mittens","Mitzi","Mitzy","Mo","Mocha","Mollie","Molly","Mona","Muffy","Nakita","Nala","Nana","Natasha","Nellie","Nemo","Nena","Peanut","Peanuts","Pearl","Pebbles","Penny","Phoebe","Phoenix","Sara","Sarah","Sasha","Sassie","Sassy","Savannah","Scarlett","Shasta","Sheba","Sheena","Shelby","Shelly","Sienna","Sierra","Silky","Silver","Simone","Sissy","Skeeter","Sky","Skye","Skyler","Waldo","Wallace","Wally","Walter","Wayne","Weaver","Webster","Wesley","Westie",]
         name = random.choice(names)
         return name
+
+    # @staticmethod
+    # async def get_image():
+
